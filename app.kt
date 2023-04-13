471f412834a91c39173d5129d8cd081f80eaaf50
João Paulo Hotequil <hotequil@protonmail.com>
2023-04-12 22:08:50 -0300

    Create app.kt

---
+++ app.kt
@@ -0,0 +1,56 @@
+fun main() {
+    var age = 20
+    
+    println(age)
+    
+    age = 19
+    
+    println(age)
+    
+    val productPrice = 50.9
+    val city = "Pomerode"
+    
+    println(productPrice)
+    println(city)
+    
+    var children: Int = 4
+    
+    println(children)
+    
+    var height: Double = 1.8
+    
+    println(height)
+    
+    var isMarried = true
+    
+    println(isMarried)
+    
+    var hasHouse: Boolean = true
+    
+    println(hasHouse)
+    
+    var name = "João Paulo"
+    var surname: String = "Hotequil"
+    
+    println(name)
+    println(surname)
+    println(name.uppercase())
+    println(surname.lowercase())
+    
+    var gender: Char = 'M'
+    
+    println(gender)
+    
+    var (address, number) = Pair("Street", 150)
+    
+    println(address)
+    println(number)
+    
+    var myData: Int? = null
+    
+    println(myData)
+    
+    myData = 10
+    
+    println(myData)
+}
